f304c83708717285c900e78db1ff409b13ce43b4
Noahoneal001 <143848565+Noahoneal001@users.noreply.github.com>
2024-04-08 14:01:17 -0700

    A

--- brainGoBrr.js
+++ brainGoBrr.js
@@ -1,18 +1,55 @@
 import * as THREE from 'three';
 
+import {OBJLoader} from 'three/addons/loaders/OBJLoader.js';
+import {MTLLoader} from 'three/addons/loaders/MTLLoader.js';
+
+let hello;
+const canvas = document.querySelector(`#threejsLoader`);
+const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
+const scene = new THREE.Scene();
+renderer.render(scene, camera);
+camera.position.z = 2;
+
+scene.background = new THREE.Color(0xAAAAAA);
+
+const color = 0xFFFFFF;
+const intensity = 3;
+const light = new THREE.DirectionalLight(color, intensity);
+light.position.set(-1, 2, 4);
+scene.add(light);
+
+const objLoader = new OBJLoader();
+const mtlLoader = new MTLLoader();
+mtlLoader.load('assets/materials.mtl', (mtl) => {
+  mtl.preload();
+  objLoader.setMaterials(mtl);
+  objLoader.load('assets/model.obj', (root) => {
+    scene.add(root);
+  });
+});
+
 document.addEventListener(`DOMContentLoaded`, function () {
 
-    const canvas = document.querySelector(`#threejsLoader`);
-    const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
-    const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 5);
-    const scene = new THREE.Scene();
-    renderer.render(scene, camera);
+        document.addEventListener(`keydown`, (eventData) => {
+            if(eventData.key == `ArrowLeft`) {camera.position.x -= .5}
+            else if(eventData.key == `ArrowRight`) {camera.position.x += .5}
+            else if(eventData.key == `ArrowUp`) {camera.position.z -= .5}
+            else if(eventData.key == `ArrowDown`) {camera.position.z += .5}
+        })
 
-});
+
+})
 
 //threejs function zone
 
+animate();
 
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
 
 //End of threejs function zone
 
