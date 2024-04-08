5c62a9cde3e2d21f5218fefedeaee575f3a58daf
Noahoneal001 <143848565+Noahoneal001@users.noreply.github.com>
2024-04-08 15:48:46 -0700

    YAH

--- brainGoBrr.js
+++ brainGoBrr.js
@@ -1,43 +1,64 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-import {OBJLoader} from 'three/addons/loaders/OBJLoader.js';
-import {MTLLoader} from 'three/addons/loaders/MTLLoader.js';
-
-let hello;
 const canvas = document.querySelector(`#threejsLoader`);
 const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
 const scene = new THREE.Scene();
 renderer.render(scene, camera);
-camera.position.z = 2;
+camera.position.y = 5;
+camera.position.z = 5;
+camera.rotation.x = -0.6;
 
-scene.background = new THREE.Color(0xAAAAAA);
+scene.background = new THREE.Color(0x00000000);
+
+const loader = new THREE.TextureLoader();
+const floorTexture = loader.load('assets/Floor.png');
+floorTexture.colorSpace = THREE.SRGBColorSpace;
+const floorSize = 40;
+const floorGeometry = new THREE.PlaneGeometry(floorSize, floorSize);
+const floorMaterial = new THREE.MeshLambertMaterial({ map: floorTexture, side: THREE.DoubleSide });
+floorTexture.wrapT = THREE.RepeatWrapping;
+floorTexture.wrapS = THREE.RepeatWrapping;
+floorTexture.magFilter = THREE.NearestFilter;
+floorTexture.colorSpace = THREE.SRGBColorSpace;
+const repeats = floorSize / 2;
+floorTexture.repeat.set(repeats, repeats);
+const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+floor.rotation.x = Math.PI * -.5;
+scene.add(floor);
+
+let table;
+
+const gltfLoader = new GLTFLoader();
+const url = 'assets/chess4_5.gltf';
+gltfLoader.load(url, (gltf) => {
+  const root = gltf.scene;
+  scene.add(root);
+  console.log(dumpObject(root).join('\n'));
+  table = root.getObjectByName('Scene');
+  table.position.y = 2;
+  console.log(table.position)
+});
+
 
 const color = 0xFFFFFF;
-const intensity = 3;
+const intensity = 2;
 const light = new THREE.DirectionalLight(color, intensity);
-light.position.set(-1, 2, 4);
+light.position.set(0, 3, 0);
 scene.add(light);
 
-const objLoader = new OBJLoader();
-const mtlLoader = new MTLLoader();
-mtlLoader.load('assets/materials.mtl', (mtl) => {
-  mtl.preload();
-  objLoader.setMaterials(mtl);
-  objLoader.load('assets/model.obj', (root) => {
-    scene.add(root);
-  });
-});
 
 document.addEventListener(`DOMContentLoaded`, function () {
 
-        document.addEventListener(`keydown`, (eventData) => {
-            if(eventData.key == `ArrowLeft`) {camera.position.x -= .5}
-            else if(eventData.key == `ArrowRight`) {camera.position.x += .5}
-            else if(eventData.key == `ArrowUp`) {camera.position.z -= .5}
-            else if(eventData.key == `ArrowDown`) {camera.position.z += .5}
-        })
+  document.addEventListener(`keydown`, (eventData) => {
+    if (eventData.key == `ArrowLeft`) { camera.position.x -= .5 }
+    else if (eventData.key == `ArrowRight`) { camera.position.x += .5 }
+    else if (eventData.key == `ArrowUp`) { camera.position.y -= .5 }
+    else if (eventData.key == `ArrowDown`) { camera.position.y += .5 }
+    else if (eventData.key == `v`) { console.log(camera.rotation); console.log(camera.position) }
+  })
 
 
 })
@@ -47,8 +68,8 @@ document.addEventListener(`DOMContentLoaded`, function () {
 animate();
 
 function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
 }
 
 //End of threejs function zone
@@ -57,11 +78,23 @@ function animate() {
 //DOMContent function zone
 
 function elementById(id) {
-    return document.getElementById(id);
+  return document.getElementById(id);
 };
 
 function addListener(id, event, code) {
-    elementById(id).addEventListener(event, (eventData) => { code(); });
+  elementById(id).addEventListener(event, (eventData) => { code(); });
 };
 
 //End of DOMContent funtion zone
+
+function dumpObject(obj, lines = [], isLast = true, prefix = '') {
+  const localPrefix = isLast ? '└─' : '├─';
+  lines.push(`${prefix}${prefix ? localPrefix : ''}${obj.name || '*no-name*'} [${obj.type}]`);
+  const newPrefix = prefix + (isLast ? '  ' : '│ ');
+  const lastNdx = obj.children.length - 1;
+  obj.children.forEach((child, ndx) => {
+    const isLast = ndx === lastNdx;
+    dumpObject(child, lines, isLast, newPrefix);
+  });
+  return lines;
+}
